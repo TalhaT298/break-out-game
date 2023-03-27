5d9fb2d0acdf881f5192b6f7de80a4c0cc49552a
Talha <mdtalha2008@gmail.com>
2023-03-28 02:11:24 +0600

    done

---
+++ src/components/games/breakout/PlayerStats.js
@@ -0,0 +1,17 @@
+export default function PlayerStates(ctx, player, canvas) {
+    ctx.font = '20px Arial';
+    ctx.fillStyle = "white";
+    ctx.fillText(`Level: ${player.level}`, 20, 30);
+
+    ctx.font = "20px Arial"
+    ctx.fillStyle = "red"
+    let gap = 0;
+    for (let i = 0; i < player.lives; i++) {
+        ctx.fillText("💙", canvas.width / 2 + gap, 30);
+        gap += 30;
+    }
+    
+    ctx.font = "20px Arial";
+    ctx.fillStyle = 'white';
+    ctx.fillText(`Score: ${player.score}`, canvas.width - 140, 30);
+}
